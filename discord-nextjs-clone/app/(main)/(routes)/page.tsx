6ba228d3dc2d0ae5c82af6107b5fbb0a2de28754
yanpatla <tomipatlayan@gmail.com>
2023-08-26 18:21:06 +0300

    dark mode

--- discord-nextjs-clone/app/(main)/(routes)/page.tsx
+++ discord-nextjs-clone/app/(main)/(routes)/page.tsx
@@ -1,8 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-       This is protected route
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
