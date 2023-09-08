975b98891b99de8bda33ba5a557a903fab1198eb
yanpatla <tomipatlayan@gmail.com>
2023-09-08 15:26:11 +0300

    form and part of the uplading image

---
+++ discord-nextjs-clone/app/(setup)/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import InitialModal from "@/components/modals/initial-modal";
+const SetUpPage = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.profile.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  return <InitialModal/>;
+};
+
+export default SetUpPage;
